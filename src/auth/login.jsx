1511f81c18e5e71e2c938fce5c507ba1e13270ba
Ayush-kathayat <kathayatayush789@gmail.com>
2024-07-09 15:51:40 +0530

    Added the frontend for login and added the title and subtitle

--- src/auth/login.jsx
+++ src/auth/login.jsx
@@ -1,6 +1,121 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
+
+// importing loader
+import { LineWave } from "react-loader-spinner";
+
+import { Link } from "react-router-dom";
+
 import "./login.css";
+
 const Login = () => {
-  return <div>login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [activeInputPassword, setActiveInputPassword] = useState(false);
+
+  // const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({
+  });
+
+  const onSubmit = async () => {
+   
+  };
+
+  return (
+    <>
+      <div className="login">
+        <div className="login-left">
+          <form
+            noValidate
+            className="login-form"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {/* Your input fields and submit button */}
+            <h2 className="form-title">LOGIN</h2>
+            <input
+              className="login-input-email"
+              type="email"
+              id="email"
+              placeholder="Email"
+              {...register("username", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            {errors.username && (
+              <p className="form-errors">{errors.username.message}</p>
+            )}
+            <div className="psswd-input">
+              <input
+                className="input input-pass"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                {...register("password")}
+                onChange={() => setActiveInputPassword(true)}
+              />
+
+              {activeInputPassword &&
+                (showPassword ? (
+                  <img
+                    className="eye-icon"
+                    onClick={() => setShowPassword(!showPassword)}
+                    src="./eye.svg"
+                    alt="Hide password"
+                  />
+                ) : (
+                  <img
+                    className="eye-icon"
+                    onClick={() => setShowPassword(!showPassword)}
+                    src="./eye-off.svg"
+                    alt="Show password"
+                  />
+                ))}
+            </div>
+            {errors.password && (
+              <p className="form-errors">{errors.password.message}</p>
+            )}
+            {isSubmitting ? (
+              <LineWave
+                visible={true}
+                height="100"
+                width="100"
+                color="#4fa94d"
+                ariaLabel="line-wave-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                firstLineColor=""
+                middleLineColor=""
+                lastLineColor=""
+              />
+            ) : (
+              <button
+                className="login-btn"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Login
+              </button>
+            )}
+            <Link to="/signup" className="auth-link">
+              <p>Don't have an account?</p>
+            </Link>
+          </form>
+        </div>
+        <div className="login-right"></div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,12 +1,16 @@
-
-
 import Header from "../components/header";
 import "./landingPage.css";
 
 const landingPage = () => {
   return (
     <>
-      <Header />     
+      <Header />
+      <div className="titles">
+        <h1 className="hero-title">Welcome to Job Referral Platform.</h1>
+        <h3 className="hero-subtitle">
+          Find Job Opportunities and Services with Ease!
+        </h3>
+      </div>
     </>
   );
 };
